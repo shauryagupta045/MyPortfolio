bca992ace0b94e535c87c34ebbe2c9d24c4e7c26
Shaurya Gupta <guptashaurya0728@gmail.com>
2025-04-06 12:53:45 +0530

    home page responsive corrected

--- script.js
+++ script.js
@@ -110,3 +110,10 @@ window.onload = () => {
 
       container.appendChild(card);
     });
+
+    const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+  navigation.classList.toggle('active');
+});
